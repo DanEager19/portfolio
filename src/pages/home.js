257a2bc01f09d1382f0f8e432243e9dc84888430
Daniel <daneager19@gmail.com>
2022-01-25 20:02:54 -0600

    Init commit

---
+++ src/pages/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home () {
+
+    return (
+        <div className="text-center">
+            <p>to be or not to be</p>
+        </div>
+    )
+}
